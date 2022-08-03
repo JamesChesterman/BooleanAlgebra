df4f7417a2437bea0b648bf529472fc90613ebd7
james chesterman <chesterman261@sky.com>
2022-08-03 16:12:37 +0100

    Added submit button.

--- EquationEnter.java
+++ EquationEnter.java
@@ -1,11 +1,17 @@
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.JButton;
+
+import java.awt.BorderLayout;
+import java.awt.event.*;
 
 public class EquationEnter extends JPanel{
     private int x = 0;
     private int y = 0;
     private int w;
     private int h;
+    JButton submitButton;
+    JTextField textfield;
 
     public EquationEnter(){
         w = GUI.getWIDTH();
@@ -13,9 +19,21 @@ public class EquationEnter extends JPanel{
         setBounds(x, y, w, h);
         setBackground(GUI.getLightPurple());
 
-        JTextField textfield = new JTextField(25);
+        textfield = new JTextField(25);
         textfield.setFont(GUI.getNormalFont());
         textfield.setBounds(w/10, h/10, w/2, h/3);
         add(textfield);
+
+        submitButton = new JButton("Submit");
+        submitButton.addActionListener(actions);
+        add(submitButton);
     }
+
+    private ActionListener actions = new ActionListener(){
+        public void actionPerformed(ActionEvent e){
+            if(e.getSource() == submitButton){
+                System.out.println(textfield.getText());
+            }
+        }
+    };
 }
