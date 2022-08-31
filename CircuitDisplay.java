e7e9fe577f82f1236be9d26919d845fce00ecb70
james chesterman <chesterman261@sky.com>
2022-08-31 23:12:39 +0100

    Added lines between the gates.

--- CircuitDisplay.java
+++ CircuitDisplay.java
@@ -16,6 +16,7 @@ public class CircuitDisplay extends JPanel {
     Graphics2D g2d;
     private boolean drawing;
     private BinaryTree tree;
+    private float sizeMultiplier = 1.0f;
 
     Image leftImage, rightImage, straightImage;
 
@@ -44,30 +45,40 @@ public class CircuitDisplay extends JPanel {
         
         g2d.setPaint(Color.blue);
         g2d.setStroke(new BasicStroke(5));
+        g2d.drawImage(leftImage, 0, 0, 50, 100, null);
 
         if(drawing == true){
-            drawTree(tree, w-(w/6), (h/2)-(imgHeight/2), "top");
+            drawTree(tree, 500, 300, "top");
         }
     }
 
     
     public void drawTree(BinaryTree t, int X, int Y, String direction){
         if(t != null){
+            System.out.println("x:" + X + " y:" + Y);
             if(direction == "left"){
-                g2d.drawImage(leftImage, X+(imgWidth/2), Y+(2*imgHeight/3), imgWidth, imgHeight, null);
+                g2d.drawImage(leftImage, X+imgWidth, Y+(imgHeight/2), imgWidth, imgHeight, null);
             }else if(direction == "right"){
-                g2d.drawImage(rightImage, X+(imgWidth/2), Y-(2*imgHeight/3), imgWidth, imgHeight, null);
+                g2d.drawImage(rightImage, X+imgWidth, Y-(imgHeight/2), imgWidth, imgHeight, null);
             }else if(direction == "straight"){
-                g2d.drawImage(straightImage, X+(imgWidth/2), Y, imgWidth, imgHeight, null);
+                g2d.drawImage(straightImage, X+imgWidth, Y+(imgHeight/2), imgWidth, imgHeight, null);
             }
+            //ISSUE: the gates before the current one being looked at are drawn first (before these lines too.)
             g2d.drawImage(t.gate.getImage(), X, Y, imgWidth, imgHeight, null);
             validate();
             repaint();
             if(t.right == null){
-                drawTree(t.left, X-50, Y, "straight");
+                if(t.left != null){
+                    drawTree(t.left, X-(2*imgWidth), Y, "straight");
+                }
             }else{
-                drawTree(t.left, X-50, Y-50, "left");
-                drawTree(t.right, X-50, Y+50, "right");
+                if(t.left != null){
+                    drawTree(t.left, X-(2*imgWidth), Y-imgHeight, "left");
+                }
+                if(t.right != null){
+                    drawTree(t.right, X-(2* imgWidth), Y+imgHeight, "right");
+                }
+                
             }
         }
     }
